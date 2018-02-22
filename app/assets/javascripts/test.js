cced575c6ab9fe38496561f101cacd9d8606ebe8
Narongsak <narongsak.chobsri@gmail.com>
2018-02-22 15:37:59 +0700

    Add javascript modules

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -5,13 +5,13 @@
 
 	let Myapp = window.Myapp
 	Myapp.App = Myapp.App || {}
-
+	
 	Myapp.App.Test = {
 		initTest: function() {
 			console.log("success")
 		},
 		Init: function() {
-			this.initTest()
+ 			this.initTest()
 		}
 	}
 
